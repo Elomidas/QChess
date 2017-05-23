cbd48f5d800e7e0d4ec06103300d753eca7de7b1
Martial TARDY <Martial TARDY>
2017-05-23 21:24:34 +0200

    alphabeta

--- QChess/include/AlphaBeta.h
+++ QChess/include/AlphaBeta.h
@@ -1,7 +1,8 @@
 #ifndef ALPHABETA_H
 #define ALPHABETA_H
 
-
+#include "Piece.h"
+#include "Plateau.h"
 
 #define _NB_PIECES  12
 
@@ -18,12 +19,16 @@ class AlphaBeta
         int Min_Value(Plateau state,int alpha, int beta);
         //Max
         int Max_Value(Plateau state,int alpha, int beta);
-        //Initialisation
-        Deplacement AlphaBetaDecision(Plateau state);
+        //renvoie le mouvement optimal : tab[3] = {index piece, x,y}
+        int * AlphaBetaDecision(Plateau state);
 
-        int Eval(Plateau plateau);
+        int Eval(Plateau * plateau);
 
         bool TestFinal(Plateau plateau);
+
+        int* ABMinMax(Plateau* plateau, short int depth_limit);
+        int* ABMaxMove(Plateau* plateau, short int depth_limit, short int depth, int a, int b, int * move_env);
+        int* ABMinMove(Plateau* plateau, short int depth_limit, short int depth, int a, int b, int * move_env);
     protected:
 
     private:
